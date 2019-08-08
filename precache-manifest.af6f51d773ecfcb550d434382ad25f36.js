a759512e507a74421c6fedf6814885db6438131b
afryingpan <christopher.gomez.432@my.csun.edu>
2019-08-08 12:16:04 -0700

    Updates

---
+++ precache-manifest.af6f51d773ecfcb550d434382ad25f36.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479b02343a09bd4222acc673c3f2fe66",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f7c22fd15458ace4759f",
+    "url": "/portfolio/static/css/main.89f4a374.chunk.css"
+  },
+  {
+    "revision": "02fcfcfbf7f41f08d4e2",
+    "url": "/portfolio/static/js/2.3069bb05.chunk.js"
+  },
+  {
+    "revision": "f7c22fd15458ace4759f",
+    "url": "/portfolio/static/js/main.4e127252.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "826923c7d4ab7c4ee2dc369da08a3abe",
+    "url": "/portfolio/static/media/city-def.826923c7.png"
+  },
+  {
+    "revision": "ff8cd5d33072b770a783c2bb41f623c0",
+    "url": "/portfolio/static/media/jupyter.ff8cd5d3.png"
+  },
+  {
+    "revision": "d53f2a2f9b2cb200b71bc5f13c941e85",
+    "url": "/portfolio/static/media/nba.d53f2a2f.png"
+  },
+  {
+    "revision": "d607b5e27a10f6b4180fa87cbffca711",
+    "url": "/portfolio/static/media/orb.d607b5e2.PNG"
+  },
+  {
+    "revision": "a191dee84d9f135da63491ddcb32f9d5",
+    "url": "/portfolio/static/media/resume.a191dee8.pdf"
+  },
+  {
+    "revision": "bbc03885ba0c8bc0da6a6393b615a23b",
+    "url": "/portfolio/static/media/smartdj.bbc03885.png"
+  },
+  {
+    "revision": "b79f20fcd0d4c257c6f4ea52063b9256",
+    "url": "/portfolio/static/media/smooth.b79f20fc.png"
+  },
+  {
+    "revision": "917e034a511e499ed9658da90b942433",
+    "url": "/portfolio/static/media/spotilize.917e034a.JPG"
+  },
+  {
+    "revision": "fe0613e0bc55e378315ec55a59a4f753",
+    "url": "/portfolio/static/media/syn_orange.fe0613e0.png"
+  }
+]);
